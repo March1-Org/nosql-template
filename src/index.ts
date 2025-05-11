314f7c03f78a24ffa7e4e666e3f356879b94f109
bonzo4 <alonzo.sabino814@gmail.com>
2025-05-10 22:30:38 -0500

    feat: export user

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,3 @@
-import { connect } from 'db';
 import { User } from 'schema/user';
 
-await connect();
-
-const newUser = new User({
-  id: '1',
-  name: 'Alonzo',
-  age: 24,
-  email: 'alonzo@email.com',
-  createdAt: new Date(),
-});
-
-await newUser.save();
-
-const users = await User.find();
-console.log(users);
+export { User };
--- src/schema/user.ts
+++ src/schema/user.ts
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 
-export const schema = new mongoose.Schema({
+const schema = new mongoose.Schema({
   id: Number,
   name: String,
   age: Number,
