c0a1be51af5be6e7d6eda0ceec25904df829b0f3
bonzo4 <alonzo.sabino814@gmail.com>
2025-05-12 20:03:49 -0500

    fix: removed auth for now

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,7 +1,7 @@
 import { config } from 'config';
 import mongoose from 'mongoose';
 
-const connectionString = `mongodb://${config.MONGO_USER}:${config.MONGO_PASSWORD}@${config.MONGO_HOST}:${config.MONGO_PORT}/${config.MONGO_DB}?authSource=${config.MONGO_DB}`;
+const connectionString = `mongodb://${config.MONGO_HOST}:${config.MONGO_PORT}/${config.MONGO_DB}?authSource=${config.MONGO_DB}`;
 
 export async function connect() {
   return await mongoose.connect(connectionString);
